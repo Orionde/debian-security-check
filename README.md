27b970df331e059b9f9abbf16d22c9f96ef41981
Jorane Congio <jorane.congio@oxalide.com>
2019-03-13 10:30:48 +0100

    Update : now only add new DLA / DSA in XML file, XML file contains only new DLA / DSA

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
 # debian-security-check
 ### Utilité
 Génère un fichier XLM ayant pour but d'être interprété par [Spacewalk](http://spacewalk.redhat.com/)
+
+Le fichier XML est généré d'après les dernières DLA / DSA traitées au run précédent. Si le script n'a jamais  ajouter manuellement dans last_DXA
+
 ### Installation
 
-1) Cloner repo :
+1) Cloner le repo git de Debian:
 ```bash
-git clone https://github.com/congiohj/debian-security-check
-```
-2) Aller dans le repo :
-```bash
-cd debian-security-check
-```
-3) Cloner le repo svn de Debian:
-```bash
-# apt-get install subversion (pour svn si pas installé)
-# Penser à ouvir les ports de svn : 3890 en TCP / UDP
-svn co svn://anonscm.debian.org/svn/secure-testing
+git clone salsa.debian.org/security-tracker-team/security-tracker.git
 ```
 
-4) Installer les dépendances Python
+2) Installer les dépendances Python
 ```bash
-apt-get install python-bs4
+apt update && apt install python-bs4
 ```
 
-5) Lancer le script :
+3) Lancer le script :
 ```bash
-rm XML  # Supprimer l'ancien XML : ouvert en mode append
-python debian-security-check.py
+python3 debian-security-check.py
 ```
 
-6) Nota :
+4) Nota :
 > Pas de fichier de log
 
 > Les DSA / DLA d'avant 2008 ne sont pas traitées correctement
--- main.py
+++ main.py
@@ -4,40 +4,49 @@ import re
 import subprocess
 import sys
 
-from xml.sax.saxutils import quoteattr # Escape " and ' in XML
-
 import DXA
-import CVE
+
+from xml.sax.saxutils import quoteattr # Escape " and ' in XML
 
 def generate_new_DXA(last_DLA, last_DSA):
 	DXA_array = list()
+	first_DLA = ""
+	first = True
+
+	#import pdb; pdb.set_trace()
 	with open('security-tracker/data/DLA/list') as DLA_file:
 		for line in DLA_file:
 			if last_DLA in line:  # add only new DLA
-				print("last")
 				break
 			try:
-				DLA_date = re.search(r'^\[.*([0-8]{4})\]', line).group(1)
 				DLA_name = re.search('DLA-[0-9]*-[0-9]*', line).group(0)
+				if first:
+					first_DLA = DLA_name
+					first = False
+				DLA_date = re.search(r'^\[([^\]]*)\]*', line).group(1)
 				DLA_soft = re.match(r'\[.*\] DLA-[0-9\-]* (.*) -.*', line).group(1)
 				DLA_soft = DLA_soft.replace(" ", "")
 				DLA_syno = re.search(" - (.*)$", line).group(1)
 				nex = next(DLA_file)  # Look next line of file for corresponding CVE
 				CVE_names = re.findall(r'CVE-[0-9]*-[0-9]*', nex)
-				print("CVE : ", CVE_names)
 				DLA_synopsys = DLA_soft + " -- " + DLA_syno
 				# Add new object to DLA array
 				DXA_array.append(DXA.DXA('DLA', DLA_name, DLA_soft, DLA_date, CVE_names, DLA_synopsys))
 			except AttributeError:  # Regex will not always match
 				pass
 
+	first_DSA = ""
+	first = True
 	with open("security-tracker/data/DSA/list", "r") as DSA_file:
 			for line in DSA_file:
 				if last_DSA in line:
 					break
 				try:
-					DSA_date = re.search(r'^\[.*([0-8]{4})\]', line).group(1)
 					DSA_name = re.search('DSA-[0-9]*-[0-9]*', line).group(0)
+					if first:
+						first_DSA = DSA_name
+						first = False
+					DSA_date = re.search(r'^\[([^\]]*)\]*', line).group(1)
 					DSA_soft = re.match(r'\[.*\] DSA-[0-9\-]* (.*) -.*', line).group(1)
 					DSA_soft.replace(" ", "")
 					DLA_syno = re.search(" - (.*)$", line).group(1)
@@ -49,120 +58,77 @@ def generate_new_DXA(last_DLA, last_DSA):
 					DXA_array.append(DXA.DXA('DSA', DSA_name, DSA_soft, DSA_date, CVE_names, DSA_synopsys))
 				except AttributeError:  # Regex will not always match
 					pass
-	return DXA_array
+	return DXA_array, first_DLA, first_DSA
 
-def create_xml_file():
+def create_xml_file(DXA_array):
 	with open('XML', 'a') as xml_file:
 		xml_file.write('<?xml version="1.0"?>\n')
 		xml_file.write('<opt>\n')
-		for dla in DXA_array:
-			to_write = '  <' + quoteattr(dla.name) + ' description=' + quoteattr(dla.description) \
-				+ ' from="Debian CVS english security report" multirelease="1" notes="' \
-				+ quoteattr(dla.notes) + '" product="Debian Linux" references="' \
-				+ quoteattr(dla.link) + '" release="1" solution="Not available" synopsis="' \
-				+ quoteattr(dla.synopsys) + '" topic="' + quoteattr(dla.synopsys) \
-				+ '" type="Security Advisory" security_name="' + quoteattr(dla.name) + '">\n'
+		for dxa in DXA_array:
+			to_write = '  <' + dxa.name + ' description=' + quoteattr(dxa.description) \
+				+ ' from="Debian CVS english security report" multirelease="1" notes=' \
+				+ quoteattr(dxa.notes) + ' product="Debian Linux" references=' \
+				+ quoteattr(dxa.link) + ' release="1" solution="Not available" synopsis=' \
+				+ quoteattr(dxa.synopsys) + ' topic=' + quoteattr(dxa.synopsys) \
+				+ ' type="Security Advisory" security_name=' + quoteattr(dxa.name) + '>\n'
 			xml_file.write(to_write)
-			for pack in dla.packages:
-				for version in dla.versions:
+			for pack in dxa.packages:
+				for version in dxa.versions:
 					to_write = '    <packages>' + pack + '-' + version + '.amd64-deb.deb</packages>\n'
 					xml_file.write(to_write)
-			for cve in dla.CVE:
-				to_write = '    <cves>' + cve.name + '</cves>\n'
-				xml_file.write(to_write)
+			if dxa.CVE:
+				for cve in dxa.CVE:
+					to_write = '    <cves>' + cve + '</cves>\n'
+					xml_file.write(to_write)
 				
-			to_write = '  </' + dla.name + '>\n'	
+			to_write = '  </' + dxa.name + '>\n'	
 			xml_file.write(to_write)
-		xml_file.write('</opt>')
+		xml_file.write('</opt>\n')
 
-def complete_XML_file(DXA_array, last_DSA, last_DLA):
+def write_last_DXA(first_DLA, first_DSA):
+	# Format : text, like
+	# DSA-2019-12
+	# DLA-2019-13	
+	# Conserve last 2 versions
+	to_write = last_DLA + " " + last_DSA + "\n"
+	with open('last_DXA', 'w') as last_DXA:
+		last_DXA.write(to_write)
+
+def get_latest_DXA():
 	"""
-	Open current XML file and add new DLA / DSA.
-	1. Calculate text of new DLA / DSA
-	2. Transform current XML file in array
-	3. Find the latests DLA / DSA presents in current XML file
-	4. Use the last DSA / DLA to know where to add the NEW DLA / DXA text in array
-	5. Write array in file
+	Parse text file last_DXA and return latest DLA and DSA name
+	File content example :
+		DLA-1708-1 DSA-4405-1
+	Out :
+		String last_DLA
+		String last_DSA
 	"""
 
-	# Generate XML text and adding it in lists (one for DLA, one for DSA)
-	new_DSA = list()
-	new_DLA = list()
-	for dxa in DXA_array:
-		to_write = '  <' + dxa.name + ' description=' + quoteattr(dxa.description) \
-			+ ' from="Debian CVS english security report" multirelease="1" notes=' \
-			+ quoteattr(dxa.notes) + ' product="Debian Linux" references=' \
-			+ quoteattr(dxa.link) + ' release="1" solution="Not available" synopsis=' \
-			+ quoteattr(dxa.synopsys) + ' topic=' + quoteattr(dxa.synopsys) \
-			+ ' type="Security Advisory" security_name=' + quoteattr(dxa.name) + '>\n'
-		new_DSA.append(to_write) if dxa.typ == 'DSA' else new_DLA.append(to_write)  # Adding to the correct list
+	if os.path.isfile('last_DXA'):
+		with open('last_DXA') as last_DXA:
+			lines = last_DXA.read().split()
+	else:
+		print("Missing file 'last_DXA' !")
+		sys.exit(1)
+	if len(lines) == 2:
+		if "DLA" in lines[0] and "DSA" in lines[1]:
+			return lines[0],lines[1]
 
-		for pack in dxa.packages:
-			for version in dxa.versions:
-				to_write = '    <packages>' + pack + '-' + version + '.amd64-deb.deb</packages>\n'
-				new_DSA.append(to_write) if dxa.typ == 'DSA' else new_DLA.append(to_write)  # Adding to the correct list
-
-		for cve in dxa.CVE:
-			to_write = '    <cves>' + cve + '</cves>\n'
-			new_DSA.append(to_write) if dxa.typ == 'DSA' else new_DLA.append(to_write)  # Adding to the correct list
-
-		to_write = '  </' + dxa.name + '>\n'	
-		new_DSA.append(to_write) if dxa.typ == 'DSA' else new_DLA.append(to_write)  # Adding to the correct list
-
-
-	# Transoform current XML file in array	
-	file_array = list()
-	with open('XML', 'r') as xml_file:
-		for l in xml_file: file_array.append(l)
-
-
-	index_first_DSA = file_array.index('<opt>\n') + 1  # First DLA (Always after the opt mark)
-	index_first_DLA = file_array.index('  </DSA-542-1>\n')  # First DSA (Will never change)
-
-	# Add DLA text in XML file using index
-	for i in new_DLA:
-		file_array.insert(index_first_DLA, i)
-		index_first_DLA += 1
-	
-	# Add DSA text in XML file using index
-	for i in new_DSA:
-		file_array.insert(index_first_DSA, i)
-		index_first_DSA += 1
-
-	# Write the XML file with new DLA / DSA calculated below
-	with open('XML', 'w') as xml_file:
-		for i in file_array:
-			xml_file.write(i)
-
-def find_last_DXA():
-	last_DSA = str
-	last_DLA = str
-	with open('XML', 'r') as xml_data:
-		for d in xml_data:
-			if '<DSA' in d:
-				last_DSA = d.split()[0].replace('<', '')
-				break
+	# No need to add if condition because of return
+	print("File last_DXA malformed")
+	sys.exit(1)
 			
-		for d in xml_data:
-			if '<DLA' in d:
-				last_DLA = d.split()[0].replace('<', '')
-				break
-
-	return last_DSA, last_DLA
 		
 if __name__ == '__main__':
-	CVE_array = list()
-	if os.path.isfile('XML'):
-		out = subprocess.check_output(['git', 'pull'], cwd="security-tracker")
-		#if "security-tracker/" in out:
-		CVE_array = CVE.generate_CVE()
-		last_DLA, last_DSA = find_last_DXA()
-		DXA_array = generate_new_DXA(last_DSA, last_DLA)
-		DXA.set_missing(DXA_array, CVE_array)
-		complete_XML_file(DXA_array, last_DSA, last_DLA)
-		sys.exit(0) # Nothing to do
+	"""
+	"""
 
-	else:
-		print("File not exists")
-		os.system("svn update security-tracker")
-		CVE_array = CVE.generate_CVE()
+	# TODO : add condition to check if git pull succeed
+	if os.path.isfile("XML"):
+		os.rename("XML", "XML.bak")
+		
+	last_DLA, last_DSA = get_latest_DXA()
+	out = subprocess.check_output(['git', 'pull'], cwd="security-tracker")
+	DXA_array, last_DLA, last_DSA = generate_new_DXA(last_DLA, last_DSA)
+	create_xml_file(DXA_array)
+	write_last_DXA(last_DLA, last_DSA)
